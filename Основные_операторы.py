2fafec3c9ffa5ac258077b03ce64037914e5cad5
DanteDisward <94927935+DanteDisward@users.noreply.github.com>
2024-08-26 00:20:18 +0500

    Убрано бесполезное условие

--- Основные_операторы.py
+++ Основные_операторы.py
@@ -4,10 +4,8 @@ n = random.choice(range(3, 21))
 print(n)
 gt = []
 for i in range(1, n):
-    for j in range(i, n):
-        if i == j:
-            continue #Пропускаем пару, если числа равны между собой или их сумма больше указанного числа
-        elif n % (i + j) == 0:
+    for j in range(i + 1, n):
+        if n % (i + j) == 0:
             gt.append([str(i), str(j)]) #Записываем пару в словарь, если пара кратна указанному числу и этой пары нет в списке
 password = ''
 for k in gt:
